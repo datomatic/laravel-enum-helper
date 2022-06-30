e6c409cc194d3a240fcc73300b44ccef63df1450
trippo <trippo@users.noreply.github.com>
2022-06-30 12:01:01 +0000

    Fix styling

--- src/Exceptions/TranslationMissing.php
+++ src/Exceptions/TranslationMissing.php
@@ -10,6 +10,6 @@ class TranslationMissing extends RuntimeException
 {
     public function __construct(\UnitEnum $enum, string $method)
     {
-        parent::__construct("Translation missing for '$method' property of ".get_class($enum)."::$enum->name");
+        parent::__construct("Translation missing for '$method' property of " . get_class($enum) . "::$enum->name");
     }
 }
